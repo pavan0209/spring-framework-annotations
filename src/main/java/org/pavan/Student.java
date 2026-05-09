7c016c8c127572cdc9fe23aa79a809386404a092
pavan0209 <pavanasonawane@gmail.com>
2026-05-09 13:31:49 +0530

    adding @Value annotation to fields

--- src/main/java/org/pavan/Student.java
+++ src/main/java/org/pavan/Student.java
@@ -4,21 +4,23 @@ import org.springframework.beans.factory.annotation.Value;
 
 public class Student {
 
+    @Value("${student.name}")
     private String name;
+
+    @Value("${student.interestedCourse}")
     private String interestedCourse;
+
+    @Value("${student.hobby}")
     private String hobby;
 
-    @Value("${student.name}")
     public void setName(String name) {
         this.name = name;
     }
 
-    @Value("${student.interestedCourse}")
     public void setInterestedCourse(String interestedCourse) {
         this.interestedCourse = interestedCourse;
     }
 
-    @Value("${student.hobby}")
     public void setHobby(String hobby) {
         this.hobby = hobby;
     }
