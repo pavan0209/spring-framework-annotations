87d32fe4cf4ea63716bc1092a3945284fec98810
pavan0209 <pavanasonawane@gmail.com>
2026-05-09 15:30:43 +0530

    getting college bean without requiring beans.xml

--- src/main/java/org/pavan/Main.java
+++ src/main/java/org/pavan/Main.java
@@ -1,15 +1,18 @@
 package org.pavan;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+//        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+//
+//        Student pavan = context.getBean("student", Student.class);
+//        pavan.displayInfo();
 
-        Student pavan = context.getBean("student", Student.class);
-        pavan.displayInfo();
+        ApplicationContext context = new AnnotationConfigApplicationContext(CollegeConfig.class);
 
         College college = context.getBean("collegeBean", College.class);
         System.out.println(college);
