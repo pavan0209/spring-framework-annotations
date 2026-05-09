b5cdeb674d595c16bbee86846d1202d2f2d050ef
pavan0209 <pavanasonawane@gmail.com>
2026-05-09 13:21:13 +0530

    configured Student bean properties using @Value annotation

--- src/main/java/org/pavan/Student.java
+++ src/main/java/org/pavan/Student.java
@@ -1,33 +1,38 @@
 package org.pavan;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class Student {
 
     private String name;
     private String interestedCourse;
     private String hobby;
 
-    public String getName() {
-        return name;
-    }
-
+    @Value("${student.name}")
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Value("${student.interestedCourse}")
+    public void setInterestedCourse(String interestedCourse) {
+        this.interestedCourse = interestedCourse;
+    }
+
+    @Value("${student.hobby}")
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public String getInterestedCourse() {
         return interestedCourse;
     }
 
-    public void setInterestedCourse(String interestedCourse) {
-        this.interestedCourse = interestedCourse;
-    }
-
     public String getHobby() {
         return hobby;
-    }
-
-    public void setHobby(String hobby) {
-        this.hobby = hobby;
     }
 
     public void displayInfo() {
